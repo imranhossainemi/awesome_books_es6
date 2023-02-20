faf2bed5adb2311e59e825749013a9e3a1886512
imranhossainemi <blur-imran@hotmail.com>
2023-02-21 02:37:20 +0600

    Update variables.js

---
+++ modules/variables.js
@@ -0,0 +1,10 @@
+const booksSec = document.querySelector('.books');
+const bookCreate = document.querySelector('.book-create');
+const contact = document.querySelector('.contact');
+const booksLink = document.getElementById('books');
+const addBookLink = document.getElementById('add-book');
+const contactLink = document.getElementById('contact');
+
+export {
+  booksSec, bookCreate, booksLink, contact, addBookLink, contactLink,
+};
