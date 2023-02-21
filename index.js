d6c50d1fa5ce02cfab64c71034e58935bf0416a1
imranhossainemi <blur-imran@hotmail.com>
2023-02-21 07:33:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import Book from './modules/book.js';
 import navigation from './modules/nav.js';
 import * as dateTime from './modules/date.js';
-
 const date = document.querySelector('.our-date');
 date.innerHTML = `${dateTime.date} ${dateTime.hour}:${dateTime.min}:${dateTime.sec}`;
-
 const awesomeBook = new Book();
-
 awesomeBook.addBook();
 awesomeBook.showBooks();
 navigation();
