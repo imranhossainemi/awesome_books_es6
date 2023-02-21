10e993fcef5bc49e38a1f1c2ce838600e584aaad
imranhossainemi <blur-imran@hotmail.com>
2023-02-21 08:45:12 +0600

    Update date.js

--- modules/date.js
+++ modules/date.js
@@ -1,8 +1,8 @@
-let luxon;
-const date = luxon.DateTime.local().toLocaleString(luxon.DateTime.DATE_MED);
-const { hour } = luxon.DateTime.local().c;
-const min = luxon.DateTime.local().c.minute;
-const sec = luxon.DateTime.local().c.second;
+let DateTime = luxon.DateTime;
+const date = DateTime.local().toLocaleString(DateTime.DATE_MED);
+const { hour } = DateTime.local().c;
+const min = DateTime.local().c.minute;
+const sec = DateTime.local().c.second;
 export {
   date, hour, min, sec,
 };
